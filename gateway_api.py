39c8f4c6cef389a52a36f6f968fbe24503e7976d
Mateusz Zawadzki <114954374+Mat-Zawadzki@users.noreply.github.com>
2023-10-20 16:10:05 +0100

    Add files via upload

---
+++ gateway_api.py
@@ -0,0 +1,22 @@
+#%%
+import requests
+import json
+
+example_df = {"index": 1, "name": "Maya", "age": 25, "role": "engineer"}
+
+invoke_url = "https://qsmuml0e2h.execute-api.us-east-1.amazonaws.com/test/12570a5c330b"
+
+#To send JSON messages you need to follow this structure
+payload = json.dumps({
+    "records": [
+        {
+        #Data should be send as pairs of column_name:value, with different columns separated by commas       
+        "value": {"index": example_df["index"], "name": example_df["name"], "age": example_df["age"], "role": example_df["role"]}
+        }
+    ]
+})
+
+headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
+response = requests.request("POST", invoke_url, headers=headers, data=payload)
+print(response.status_code)
+#%%
